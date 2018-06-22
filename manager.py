113ce977c98ffdd1f83f78b58d35139b8829fe86
benjamin <banker@cnm.edu>
2018-06-22 13:36:05 -0600

    started parrallelization. pipes breaking

---
+++ manager.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 21 15:33:43 2018
+
+@author: benjamin
+"""
+
+from multiprocessing import Process, Pipe
+from word_count import make_stats
+import time
+
+start=time.time()
+if __name__=="__main__":
+    max_processes=2
+    num_files=2
+    pipes=[]
+    processes=[]
+    for i in range(max_processes):
+        pipes.append(Pipe())
+        processes.append(Process(target=make_stats, args=(int(num_files/max_processes)*i,int(num_files/max_processes)*(i+1), pipes[i][1])))
+        processes[i].start()
+    for i in processes:
+        i.join()
+end=time.time()
+print("total time:")
+print(end-start)
--- word_count.py
+++ word_count.py
@@ -11,34 +11,60 @@ import re
 from collections import defaultdict as dd
 from nltk import word_tokenize, pos_tag
 from nltk.probability import FreqDist as fd
+import time
 #from nltk.corpus import stopwords
 
 site_list=["atlantic", "breitbart", "motherjones", "thehill"]
+#site_list=["test1", "test2"]
 direc_prefix="/home/benjamin/sfi/project_stuff/SFI_Comments_REU/sample/"
 
 def trim(word):
     return(re.sub(r'[^A-Za-z \']', '', word))
+    
+#this is a hack to get around the fact that multiprocessing uses pickle 
+#(instead of dill) and pickle can't handle lambdas
+def dd_of_fd():
+    return dd(fd)
 
-def make_stats():
+#the lowth through the hith files. eg 210, 365 would be the last four months of the year
+def make_stats(low=0, hi="hi", pipe=None):
+    start=time.time()
     word_counts=dd(fd)
-    word_POS_counts=dd(lambda: dd(fd))
+    word_POS_counts=dd(dd_of_fd)
     for site in site_list:
         direc=direc_prefix+site+"/"
+        #slightly hacky. please fix
+        i=0
         for filename in os.listdir(direc):
             if filename.startswith("comment"):
-                with open(direc+filename, "rb") as comment_list_file:
-                    comment_list=pickle.load(comment_list_file)
-                    for comment in comment_list:
-                        tagged_comment=pos_tag(word_tokenize(comment["raw_message"]))
-                        for word_POS_pair in tagged_comment:
-                                #was trimming, but i think that will just mess up
-                                #the tagging
-                                #well, the stemmer was working less well that i hoped
-                                base_word=trim(word_POS_pair[0].lower())#stemmer.stem(word_POS_pair[0].lower())
-                                word_counts[site][base_word]+=1
-                                word_POS_counts[site][base_word][word_POS_pair[1]]+=1
-    pickle.dump([word_counts, word_POS_counts], open("/home/benjamin/sfi/project_stuff/data/word_diffs.pkl", "wb"))
-    return [word_counts, word_POS_counts]
+                if hi=="hi" or i in range(low, hi):
+                    with open(direc+filename, "rb") as comment_list_file:
+                        comment_list=pickle.load(comment_list_file)
+                        for comment in comment_list:
+                            tagged_comment=pos_tag(word_tokenize(comment["raw_message"]))
+                            for word_POS_pair in tagged_comment:
+                                    #was trimming, but i think that will just mess up
+                                    #the tagging
+                                    #well, the stemmer was working less well that i hoped
+                                    base_word=trim(word_POS_pair[0].lower())#stemmer.stem(word_POS_pair[0].lower())
+                                    word_counts[site][base_word]+=1
+                                    word_POS_counts[site][base_word][word_POS_pair[1]]+=1
+                i+=1
+    pickle.dump([word_counts, word_POS_counts], open("/home/benjamin/sfi/project_stuff/data/analysis_data"+str(low)+".pkl", "wb"))
+    #if we called this as part of a multiprocess
+    if pipe is not None:
+        print("piping "+str(low)+" after time:")
+        begin_piping=time.time()
+        print(begin_piping-start)
+        pipe.send([word_counts, word_POS_counts])
+        #pipe.send(word_counts)
+        #pipe.send(word_POS_counts)
+        pipe.close()
+        print("time to pipe "+str(low)+":")
+        print(time.time()-begin_piping)
+        return
+    else:
+        return [word_counts, word_POS_counts]
 
 def proportional_diffs(word_counts, word_POS_counts):                                      
     word_diffs=dd(lambda : dd(int))

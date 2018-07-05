e2c4dbb14aeef9d97b118a290520ea3dab46a6c4
benjamin <banker@cnm.edu>
2018-07-05 09:30:16 -0600

    big refactor

--- manager.py
+++ manager.py
@@ -5,56 +5,67 @@ Created on Thu Jun 21 15:33:43 2018
 
 @author: benjamin
 """
-import multiprocessing
-from multiprocessing import Process, Queue
-from word_count import make_stats
+from multiprocessing import Process, Queue, cpu_count
+from better_word_count import make_stats
+from nltk.probability import FreqDist as fd
+from collections import defaultdict as dd
 from logger import logger
+import dill as pickle
 import time
 import os
 import re
-import random
-import pickle
-#import sys
+
+config_file=os.getcwd()+"/../locations.conf"
+f=open(config_file, "r")
+a=f.read().split("\n")
+direc_prefix=a[0]
+site_list=["atlantic", "breitbart", "motherjones", "thehill"]
+result_storage_direc=a[1]
+
+max_processes=cpu_count()-1
+
+pattern=re.compile("^comment.*")
+files=[]
+for d in site_list:
+    for i in os.listdir(direc_prefix+d+"/"):
+        if pattern.match(i):
+            files.append(direc_prefix+d+"/"+i)
+            
+#site, year, month, word
+word_counts=dd(lambda: dd(lambda: dd(fd)))
+#site, year, month, word, POS
+word_POS_counts=dd(lambda: dd(lambda: dd(lambda: dd(fd))))
+
 start=time.time()
 if __name__=="__main__":
-    config_file=os.getcwd()+"/../locations.conf"
-    f=open(config_file, "r")
-    a=f.read().split("\n")
-    direc_prefix=a[0]
-    site_list=["atlantic", "breitbart", "motherjones", "thehill"]
-    q=Queue()
-    max_processes=multiprocessing.cpu_count()-1
-    pattern=re.compile("^comment.*")
-    file_list=[]
-    for d in site_list:
-        for i in os.listdir(direc_prefix+d+"/"):
-            if pattern.match(i):
-                file_list.append(direc_prefix+d+"/"+i)
-    num_files=len(file_list)
-    to_each=int(num_files/max_processes)
-    remainder=num_files-to_each*max_processes
-    processes=[]
-    hi=0
-    #shuffle so the large files aren't next to each other
-    random.shuffle(file_list)
-    result_storage_direc=a[1]
-    with open(result_storage_direc+"file_order.pkl", "wb") as f:
-            pickle.dump(file_list, f)
-    log_process=Process(target=logger, args=(q,))
+    file_q  = Queue()
+    log_q   = Queue()
+    stats_q = Queue()
+    processed=[]
+    for i in files:
+        file_q.put(i)
+    log_process=Process(target=logger, args=(log_q,))
     log_process.start()
+    processes=[]
     for i in range(max_processes):
-        low=hi
-        hi=low+to_each
-        if remainder>0:
-            hi+=1
-            remainder-=1
-        if hi>num_files:
-            break
-        processes.append(Process(target=make_stats, args=(low, hi, file_list, num_files, q), daemon=True))
+        processes.append(Process(target=make_stats, args=(log_q, file_q, stats_q, i), daemon=True))
         processes[i].start()
+    for i in range(len(files)):
+        if i>0 and i%10==0:
+            with open(result_storage_direc+"intermediate.pkl", "wb") as f:
+                pickle.dump([word_counts, word_POS_counts, processed], f)
+        [partial_word_counts, partial_word_POS_counts, site, year, month, done_file]=stats_q.get()
+        processed.append(done_file)
+        for w in partial_word_counts.keys():
+            word_counts[site][year][month][w]+=partial_word_counts[w]
+        for w in partial_word_POS_counts.keys():
+            for p in partial_word_POS_counts[w].keys():
+                word_POS_counts[site][year][month][w][p]+=partial_word_POS_counts[w][p]
+    with open(result_storage_direc+"stats.pkl", "wb") as f:
+            pickle.dump([word_counts, word_POS_counts], f)
     for i in processes:
         i.join()
-    q.put("kill")
+    log_q.put("kill")
 end=time.time()
 print("total time:")
 print(end-start)
--- word_count.py
+++ word_count.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Tue Jul  3 14:01:03 2018
+
+@author: benjamin
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
 Created on Tue Jun 19 09:45:11 2018
 
 @author: benjamin
@@ -26,45 +34,31 @@ day_finder=re.compile(r".*_(\d{1,3})\.txt")
 year_finder=re.compile(r".*comments_(\d{4}).*")
 def trim(word):
     return(re.sub(r'[^A-Za-z \']', '', word))
-    
-#this is a hack to get around the fact that multiprocessing uses pickle 
-#(instead of dill) and pickle can't handle lambdas
-def dd_of_fd():
-    return dd(fd)
-def dd_of_dd_of_fd():
-    return dd(dd_of_fd)
-def dd_of_dd_of_dd_of_fd():
-    return dd(dd_of_dd_of_fd)
-def make_stats(low=None, hi=None,file_list=None, num_files=0, q=None):
-    word_counts=dd(dd_of_dd_of_fd)
-    word_POS_counts=dd(dd_of_dd_of_dd_of_fd)
-    try:
-        with open(result_storage_direc+"analysis_data"+str(low)+"-"+str(hi)+".pkl", "rb") as f:
-            pass
-    except:
-        for i in range(low,hi):
-            site=serP.search(file_list[i]).group(1)
-            year=year_finder.search(file_list[i]).group(1)
-            month=find_month(int(day_finder.search(file_list[i]).group(1)))
-            q.put("{}. Started {} at {}".format(i,file_list[i], time.time()))
-            print("{}. Started {} at {}".format(i,file_list[i], time.time()))
+
+def make_stats(log_q, file_q, stats_q, i):
+        while not file_q.empty():
+            c_file=file_q.get()
+            word_counts=fd()
+            word_POS_counts=dd(fd)
+            site=serP.search(c_file).group(1)
+            year=year_finder.search(c_file).group(1)
+            month=find_month(int(day_finder.search(c_file).group(1)))
+            log_q.put("Started {} at {}".format(c_file, time.time()))
+            print("Started {} at {}".format(c_file, time.time()))
             try:
-                with open(file_list[i], "rb") as comment_list_file:
+                with open(c_file, "rb") as comment_list_file:
                     comment_list=pickle.load(comment_list_file)
                     for comment in comment_list:
                         tagged_comment=pos_tag(word_tokenize(comment["raw_message"]))
                         for word_POS_pair in tagged_comment:
-                                #well, the stemmer was working less well that i hoped
                                 base_word=trim(word_POS_pair[0].lower())#stemmer.stem(word_POS_pair[0].lower())
-                                word_counts[site][year][month][base_word]+=1
-                                word_POS_counts[site][year][month][base_word][word_POS_pair[1]]+=1
+                                word_counts[base_word]+=1
+                                word_POS_counts[base_word][word_POS_pair[1]]+=1
             except:
-                q.put("Problem with file"+file_list[i]+" "+sys.exc_info()[0])
-            q.put("{}. Finished {} at {}".format(i,file_list[i], time.time()))
-            print("{}. Finished {} at {}".format(i,file_list[i], time.time()))
-        with open(result_storage_direc+"analysis_data"+str(low)+"-"+str(hi)+".pkl", "wb") as f:
-            pickle.dump([word_counts, word_POS_counts], f)
-        return [word_counts, word_POS_counts]
+                log_q.put("Problem with file "+c_file+" "+str(sys.exc_info()[0]))
+            log_q.put("Finished {} at {}".format(c_file, time.time()))
+            print("Finished {} at {}".format(c_file, time.time()))
+            stats_q.put([word_counts, word_POS_counts, site, year, month, c_file])
 
 def find_month(day):
     days_per_month=[("Jan", 31),("Feb", 28), ("Mar", 31),("Apr", 30),("May", 31),("Jun",30),("Jul",31),("Aug", 31),("Sep", 30),("Oct", 31),("Nov",30),("Dec",31)]
@@ -72,13 +66,3 @@ def find_month(day):
         day-=i
         if day<=0:
             return month
-
-def proportional_diffs(word_counts, word_POS_counts):                                      
-    word_diffs=dd(lambda : dd(int))
-    #since i'm discarding words that don't get used at least a couple of time
-    #across corpora "atlantic" could be any site (in fact, it should be the corpus with the least variety)
-    for w in word_counts["atlantic"]:
-        mi=min([word_counts[c].freq(w) for c in site_list])
-        ma=max([word_counts[c].freq(w) for c in site_list])
-        if abs(mi-ma)>0.005 and min([word_counts[c][w] for c in site_list])>3:
-            word_diffs[w]=[{c: word_counts[c].freq(w)} for c in site_list]
